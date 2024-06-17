b9e32bbaf0da14616b0fed33143f03ff8d36420a
BossNPtl <boss.dowork@gmail.com>
2024-06-17 10:17:22 +0700

    fin-comment

--- src/controllers/comment-controller.js
+++ src/controllers/comment-controller.js
@@ -4,14 +4,12 @@ const commentController = {};
 
 commentController.postComment = async (req, res, next) => {
     try {
-        console.log('%%%%%%%%%%%%%%%%%%%%%');
+        console.log('%%%%%%%%%%%%%%%%%%%%% PostComment %%%%%%%%%%%%%%%%%%%%%');
         const data = {...req.body}
         // const data = req.input
         data.user_id = req.user.id
         data.album_id = +req.params.albumId
-        console.log('data ------>>>', data)
         const post = await commentService.postComment(data);
-        console.log('post ------>>>', data)
         res.status(201).json(post);
         
     }   catch (err) {
@@ -21,11 +19,9 @@ commentController.postComment = async (req, res, next) => {
 
 commentController.getAllComment = async (req, res, next) => {
     try {
-        console.log('%%%%%%%%%%%%%%%%%%%%%');
+        console.log('%%%%%%%%%%%%%%%%%%%%% GetAllComment %%%%%%%%%%%%%%%%%%%%%');
         const album_id = +req.params.albumId;
-        console.log('album_id --->>', album_id);
         const comment = await commentService.getAllComment(album_id);
-        console.log(comment)
         res.status(200).json(comment);
     }   catch (err) {
         next(err);
@@ -34,11 +30,10 @@ commentController.getAllComment = async (req, res, next) => {
 
 commentController.editComment = async (req, res, next) => {
     try {
-        console.log('%%%%%%%%%%%%%%%%%%%%%');
-        const data = {...req.body};
-        console.log(data);
+        console.log('%%%%%%%%%%%%%%%%%%%%% Edit Comment %%%%%%%%%%%%%%%%%%%%%');
+        const data = req.body.message;
         const id = +req.params.id
-        console.log(id);
+        console.log(data, id)
         const edit = await commentService.patchComment(id, data);
         res.status(200).json(edit);
     }   catch (err) {
@@ -48,9 +43,8 @@ commentController.editComment = async (req, res, next) => {
 
 commentController.deleteComment = async (req, res, next) => {
     try {
-        console.log('%%%%%%%%%%%%%%%%%%%%%');
+        console.log('%%%%%%%%%%%%%%%%%%%%% DeleteComment %%%%%%%%%%%%%%%%%%%%%');
         const id = +req.params.id
-        console.log(id);
         const comment = await commentService.deleteComment(id);
         res.status(200).json(comment);
 
--- src/services/comment-service.js
+++ src/services/comment-service.js
@@ -5,12 +5,19 @@ const commentService = {};
 commentService.postComment = (data) => prisma.comment.create({ data });
 
 commentService.getAllComment = (albumId) => prisma.comment.findMany({
-    where: { album_id: albumId, deletedAt: null }
+    where: { album_id: albumId, deletedAt: null },
+    include: { user: true },
+    orderBy: { id: 'desc' }
 });
+
+// commentService.getAllComment = (albumId) => prisma.comment.findMany({
+//     where: { album_id: albumId, deletedAt: null },
+//     orderBy: { id: 'desc' }
+// });
 
 commentService.patchComment = (id, data) => prisma.comment.update({
     where: { id: id }, 
-    data: { message: data.message }
+    data: { message: data }
 });
 
 commentService.deleteComment = (id) => prisma.comment.update({
