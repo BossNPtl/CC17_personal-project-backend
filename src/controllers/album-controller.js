776de8c0a2ccdb564cb9645500f3b1ed3539bd61
BossNPtl <boss.dowork@gmail.com>
2024-07-19 11:12:24 +0700

    add cloudinary

--- src/controllers/album-controller.js
+++ src/controllers/album-controller.js
@@ -1,5 +1,8 @@
+const fs = require("fs");
+
 const adminService = require("../services/admin-service");
 const albumService = require("../services/album-service");
+const uploadService = require("../services/upload-service");
 const userService = require("../services/user-service");
 const createError = require("../utils/create-error");
 
@@ -9,7 +12,7 @@ albumController.getAllAlbum = async (req, res, next) => {
     try {
         const data = await albumService.getAllAlbum();
         res.status(200).json(data);
-    }   catch (err) {
+    } catch (err) {
         next(err);
     }
 };
@@ -17,10 +20,36 @@ albumController.getAllAlbum = async (req, res, next) => {
 albumController.createAlbum = async (req, res, next) => {
     try {
         const data = req.body;
+        const {
+            picture_album,
+            picture_band,
+        } = req.files;
+        console.log('data --->>', data);
+        console.log('file --->>', picture_album, picture_band);
+        console.log('req.file --->>', req.files);
+
+        const uploadFile = async (file) => {
+            const filePath = file.path;
+            const cloudinaryUrl = await uploadService.upload(filePath);
+            fs.unlink(filePath, (error) => {
+                if (error) {
+                    console.error("Error deleting file: ", filePath, error);
+                }
+            }
+            );  // Delete the local file
+            return cloudinaryUrl;
+        };
+
+        const [picture_albumUrl, picture_bandUrl] = await Promise.all([uploadFile(picture_album[0]), uploadFile(picture_band[0])]);
+        console.log('check --->', picture_album[0], picture_band[0]);
+        data.picture_album = picture_albumUrl
+        data.picture_band = picture_bandUrl
+
         const buildAlbum = await adminService.createAlbum(data);
-        
-        res.status(201).json({album:buildAlbum});
-    }   catch (err) {
+
+        res.status(201).json({ album: buildAlbum });
+    } catch (err) {
+        console.log(err)
         next(err);
     }
 };
@@ -36,7 +65,7 @@ albumController.selectAlbum = async (req, res, next) => {
         }
         res.status(200).json(existAlbum);
 
-    }   catch (err) {
+    } catch (err) {
         next(err);
     }
 };
@@ -48,7 +77,7 @@ albumController.addSong = async (req, res, next) => {
         const newSong = await adminService.addSong(data);
 
         res.status(201).json(newSong);
-    }   catch (err) {
+    } catch (err) {
         next(err);
     }
 };
@@ -64,7 +93,7 @@ albumController.getAllSong = async (req, res, next) => {
         }
         res.status(200).json(existSong);
 
-    }   catch (err) {
+    } catch (err) {
         next(err);
     }
 };
@@ -75,7 +104,7 @@ albumController.deleteAlbum = async (req, res, next) => {
         console.log(albumId)
         const album = await adminService.deleteAlbum(albumId);
         res.status(200).json(album);
-    }   catch (err) {
+    } catch (err) {
         next(err);
     }
 }
--- src/routes/album-route.js
+++ src/routes/album-route.js
@@ -2,11 +2,18 @@ const express = require('express');
 const authenticate = require('../middlewares/authenticate');
 const { checkAdminValidator } = require('../middlewares/validator-middleware');
 const albumController = require('../controllers/album-controller');
+const upload = require('../middlewares/upload-middleware');
 
 const albumRouter = express.Router();
 
 albumRouter.get('/allAlbum', albumController.getAllAlbum);
-albumRouter.post('/addAlbum', authenticate, checkAdminValidator, albumController.createAlbum);
+albumRouter.post('/addAlbum', authenticate, checkAdminValidator,
+    upload.fields([
+        { name: "picture_album", maxCount: 1 },
+        { name: "picture_band", maxCount: 1 },
+    ]),
+    albumController.createAlbum
+);
 albumRouter.get('/:albumId', albumController.selectAlbum);
 albumRouter.post('/:albumId/addSong', authenticate, checkAdminValidator, albumController.addSong);
 albumRouter.get('/:albumId/allSong', albumController.getAllSong);
